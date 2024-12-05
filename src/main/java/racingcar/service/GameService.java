f6a6d01cdc434926ca031e137c35e41040ba5b2a
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-12-05 23:30:56 +0900

    feat: 자동차들을 관리하는 기능 추가

---
+++ src/main/java/racingcar/service/GameService.java
@@ -0,0 +1,47 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameService {
+    private final static int MIN_RANDOM_RANGE = 0;
+    private final static int MAX_RANDOM_RANGE = 9;
+
+    private final List<Car> cars;
+
+    public GameService() {
+        cars = new ArrayList<>();
+    }
+
+    public void makeCars(String input) {
+        String[] carNames = input.split(",");
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public void playOneRound() {
+        for (Car car : cars) {
+            car.increaseDistance(Randoms.pickNumberInRange(MIN_RANDOM_RANGE, MAX_RANDOM_RANGE));
+        }
+    }
+
+    public List<String> calculateResult() {
+        List<String> result = new ArrayList<>();
+        int maxDistance = cars.stream().mapToInt(Car::getDistance).max().orElse(0);
+
+        for (Car car : cars) {
+            if (car.getDistance() == maxDistance) {
+                result.add(car.getName());
+            }
+        }
+        return result;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
